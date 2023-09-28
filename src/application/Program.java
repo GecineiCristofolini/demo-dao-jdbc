e98f50782fd32690151a07a804d6ffc377b1aeae
GecineiCristofolini <gecinei.c@hotmail.com>
2023-09-28 08:21:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,21 @@
 package application;
 
+
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
     Department obj = new Department(1, "Gecinei");
-    Department obj2 = new Department(1, "Gecinei");
+    Seller seller =  new Seller(1,"Joao","joao@hotmail.com",new Date(),2000.0, obj);
+
     System.out.println(obj);
-    System.out.println(obj2);
+    System.out.println(seller);
 	}
 
 }
