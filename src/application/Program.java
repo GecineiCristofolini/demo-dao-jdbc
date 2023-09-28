bac21d6b2d1e33597b998d8684c48687eb62a213
GecineiCristofolini <gecinei.c@hotmail.com>
2023-09-28 07:50:44 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+    Department obj = new Department(1, "Gecinei");
+    Department obj2 = new Department(1, "Gecinei");
+    System.out.println(obj);
+    System.out.println(obj2);
+	}
+
+}
